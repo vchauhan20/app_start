cdf96a1074cb5542cc939b3a66fdfdb3e2126b64
LAPTOP-FGPTHUNM\Karan <vijay.chauhan@plutoshift.com>
2021-02-14 17:57:10 -0500

    "docker file"

--- app.py
+++ app.py
@@ -4,12 +4,30 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from joblib import load
 
 app = Flask(__name__)
-
+new_model = create_model()
+tokenizer = load('tokenizer.pkl')
 
 @app.route('/')
 def home():
     return render_template("index.html",positive =0 , negative =0)
 
+@app.route('/predict',methods=['POST'])
+def second():
+    text = [x for x in request.form.values()]
+    processed_data = preprocess_data(text,tokenizer)
+
+    results = new_model.predict(processed_data)
+    print(results)
+    if results > .75:
+        positive = results
+        negative = 1 - positive
+    else:
+        positive = results
+        negative = 1 - positive
+    return render_template('index.html',positive = positive, negative = negative)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
---
+++ model.py
@@ -0,0 +1,14 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+def create_model():
+    model = load_model('model.hdf5')
+    return model
+
+def preprocess_data(input_str,tokenizer):
+
+    tokens = tokenizer.texts_to_sequences(input_str)
+    padded = pad_sequences(tokens, maxlen=100)
+    return padded
+
